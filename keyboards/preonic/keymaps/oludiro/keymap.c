4ed68af1d4fd1a7b127f35a07d8dd783a036c979
Oktavianus Ludiro <oktavianusl@zillowgroup.com>
2018-03-29 10:24:36 -0700

    Remove GUI Layer

--- keyboards/preonic/keymaps/oludiro/keymap.c
+++ keyboards/preonic/keymaps/oludiro/keymap.c
@@ -25,7 +25,6 @@ enum preonic_layers {
   _LOWER,
   _RAISE,
   _NAV,
-  _GUI,
   _STENO,
   _ADJUST
 };
@@ -70,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_GRV,              KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC},
-  {KC_TAB,              KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_QUOT},
-  {CTL_T(KC_ESC),       KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_NAV, KC_SCLN), CTL_T(KC_ENT)},
-  {KC_LSPO,             KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC},
-  {LT(_GUI, KC_LCBR),   ALL_T(KC_RCBR), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,             KC_RGHT}
+  {KC_GRV,        KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC},
+  {KC_TAB,        KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_QUOT},
+  {CTL_T(KC_ESC), KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_NAV, KC_SCLN), CTL_T(KC_ENT)},
+  {KC_LSPO,       KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC},
+  {KC_LCBR,       CTL_T(KC_RCBR), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,             KC_RGHT}
 },
 
 /* Dvorak
@@ -112,11 +111,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______},
-  {_______, _______, _______, _______, _______, KC_BSPC, KC_BSPC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC},
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL},
+  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
+  {KC_SLCK, KC_PAUS, KC_MPRV, KC_MNXT, _______, KC_BSPC, KC_BSPC, _______,    KC_MPLY,    KC_VOLD, KC_VOLU, KC_MUTE}
 },
 
 /* Raise
@@ -137,30 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
-  {_______, _______, _______, _______, _______, KC_DEL,  KC_DEL,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
-},
-
-/* GUI (Window management / mouse / media controls) layer
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Ms B2 |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Ms L  |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |Ms WL |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |Ms WL |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Prev | Play | Next | Brig-|    Sleep    | Brig+| Mute | Vol- | Vol+ |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_GUI] = {
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_D, _______, _______, WM_PREV, WM_NW,   WM_N,    WM_NE,   _______},
-  {_______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, _______, _______, WM_FULL, WM_W,    WM_CNTR, WM_E,    _______},
-  {_______, KC_WH_L, KC_BTN3, KC_WH_R, _______, _______, _______, WM_NEXT, WM_SW,   WM_S,    WM_SE,   _______},
-  {_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_SLCK, KC_SLEP, KC_SLEP, KC_PAUS, KC_MUTE, KC_VOLD, KC_VOLU, _______}
+  {KC_SLCK, KC_PAUS, KC_MPRV, KC_MNXT, _______, KC_DEL,  KC_DEL,  _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE}
 },
 
 /* Directional naviagtion layer
