871cfc270dacdb9d1b73be58228cf18ce3315d64
Oktavianus Ludiro <oktavianusl@zillowgroup.com>
2018-04-30 15:11:49 -0700

    Add hyper shortcut

--- keyboards/preonic/keymaps/oludiro/keymap.c
+++ keyboards/preonic/keymaps/oludiro/keymap.c
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                      Tap for [                                                       Tap for ]
  **/
 [_QWERTY] = {
-  {KC_GRV,        KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC},
-  {KC_TAB,        KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_QUOT},
-  {CTL_T(KC_ESC), KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_NAV, KC_SCLN), CTL_T(KC_ENT)},
-  {KC_LSPO,       KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC},
-  {KC_LCBR,       LCTL_T(KC_LBRC),KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, RCTL_T(KC_RBRC),   KC_RCBR}
+  {KC_GRV,               KC_1,           KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_BSPC},
+  {KC_TAB,               KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_QUOT},
+  {CTL_T(KC_ESC),        KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_NAV, KC_SCLN), CTL_T(KC_ENT)},
+  {KC_LSPO,              KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC},
+  {MT(MOD_MEH, KC_LCBR), LCTL_T(KC_LBRC),KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, RCTL_T(KC_RBRC),   MT(MOD_MEH, KC_RCBR)}
 },
 
 
